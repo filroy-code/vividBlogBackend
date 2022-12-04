940f1ba1868f736921aa03c5d536bee533f11875
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-12-04 10:03:21 -0500

    Offset of returned posts now based on a page size of 6.

--- src/database/getBlogs.ts
+++ src/database/getBlogs.ts
@@ -3,7 +3,7 @@ import { pool } from "./database.js";
 export const getBlogs = async (page: number) => {
   try {
     const sqlText = `SELECT * FROM "blogs" WHERE "published_at" IS NOT NULL ORDER BY "published_at" DESC LIMIT 6 OFFSET $1`;
-    const values = [page];
+    const values = [page * 6]; // offsets the returned results based on page size, assuming 6 posts are displayed per page
     let queryResult = await pool.query(sqlText, values);
     console.log(queryResult.rows);
     return queryResult.rows;
