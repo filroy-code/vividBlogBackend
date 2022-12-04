7b797abb5b8a9659f0215f6a96f089c38b748f98
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-12-04 12:39:53 -0500

    Configured CORS.

--- src/database/getBlogs.ts
+++ src/database/getBlogs.ts
@@ -5,7 +5,6 @@ export const getBlogs = async (page: number) => {
     const sqlText = `SELECT * FROM "blogs" WHERE "published_at" IS NOT NULL ORDER BY "published_at" DESC LIMIT 6 OFFSET $1`;
     const values = [page * 6]; // offsets the returned results based on page size, assuming 6 posts are displayed per page
     let queryResult = await pool.query(sqlText, values);
-    console.log(queryResult.rows);
     return queryResult.rows;
   } catch (err) {
     console.log(err);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import fs from "fs";
 import express, { Express, Request, Response } from "express";
-
+import cors from "cors";
 import { blogsRouter } from "./routes/blogs.js";
 
 const app: Express = express();
+
+const allowedOrigins = "http://localhost:3000";
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+  methods: "GET",
+};
+app.use(cors(options));
 
 app.use("/", blogsRouter);
 
