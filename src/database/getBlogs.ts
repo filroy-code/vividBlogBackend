b816ef9b8a7528a3476fba18d6087848339ff4a1
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-12-04 09:54:17 -0500

    Database query no longer returns unpublished blogs.

--- src/database/getBlogs.ts
+++ src/database/getBlogs.ts
@@ -2,7 +2,7 @@ import { pool } from "./database.js";
 
 export const getBlogs = async (page: number) => {
   try {
-    const sqlText = `SELECT * FROM "blogs" ORDER BY "published_at" DESC LIMIT 6 OFFSET $1`;
+    const sqlText = `SELECT * FROM "blogs" WHERE "published_at" IS NOT NULL ORDER BY "published_at" DESC LIMIT 6 OFFSET $1`;
     const values = [page];
     let queryResult = await pool.query(sqlText, values);
     console.log(queryResult.rows);
